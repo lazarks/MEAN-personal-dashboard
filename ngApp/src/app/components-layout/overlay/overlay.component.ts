0fc03b7e44c26061efb3745f21e7ec091000996e
Lazar <sljivic.lazar037@gmail.com>
2022-04-13 10:44:12 +0100

    mp test feature

--- ngApp/src/app/components-layout/overlay/overlay.component.ts
+++ ngApp/src/app/components-layout/overlay/overlay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overlay',
@@ -7,12 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverlayComponent implements OnInit {
   collapsed: boolean = true;
+  isPlaying: boolean = false;
 
-  constructor() {}
+  audio: any;
+
+  constructor(private el: ElementRef) {
+    this.audio = new Audio();
+    this.audio.src = '../../../assets/tempAudio.mp3';
+  }
 
   ngOnInit(): void {}
 
   slide() {
     this.collapsed = !this.collapsed;
+  }
+
+  playAudio(event: Event) {
+    event.stopPropagation();
+    if (this.isPlaying) {
+      this.audio.pause();
+    } else {
+      this.audio.play();
+    }
+    this.isPlaying = !this.isPlaying;
   }
 }
