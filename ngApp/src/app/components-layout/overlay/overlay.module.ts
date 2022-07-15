62b11d22c2d840e6f89a3256257bab6a74ca2d15
Lazar <sljivic.lazar037@gmail.com>
2022-07-15 15:45:22 +0200

    visualizer finished [needs design]

--- ngApp/src/app/components-layout/overlay/overlay.module.ts
+++ ngApp/src/app/components-layout/overlay/overlay.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { OverlayComponent } from './overlay.component';
 import { WeatherComponent } from './weather/weather.component';
 import { MusicPlayerComponent } from './music-player/music-player.component';
+import { VisualizerComponent } from './visualizer/visualizer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [OverlayComponent, MusicPlayerComponent, WeatherComponent],
-  imports: [CommonModule],
+  declarations: [
+    OverlayComponent,
+    MusicPlayerComponent,
+    WeatherComponent,
+    VisualizerComponent,
+  ],
+  imports: [CommonModule, FormsModule],
   exports: [OverlayComponent, MusicPlayerComponent, WeatherComponent],
 })
 export class OverlayModule {}
